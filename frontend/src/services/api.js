6f205754901dd2b1a922d70e9c62603c4cee2f1c
Ataurrahman <ataurrahman9731@gmail.com>
2026-05-09 17:19:17 +0530

    fine tune the code

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://crm-system-175m.onrender.com';
+console.log(API_URL)
 
 const api = axios.create({
     baseURL: API_URL,
